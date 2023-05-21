cfe98f92cd6709f2db1e84bac222fcb55131a989
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-21 08:38:54 +0200

    Update filter_context.js

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -12,18 +12,28 @@ import {
 } from '../actions';
 import { useProductsContext } from './products_context';
 
-const initialState = {};
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+};
 
 const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{ ...state }}>
       {children}
     </FilterContext.Provider>
   );
 };
-// make sure use
 export const useFilterContext = () => {
   return useContext(FilterContext);
 };
