715907d4b8e2af9e3be38917888cdebc9f8abbde
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-24 12:45:50 +0200

    complete all filters

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -63,10 +63,34 @@ const filter_reducer = (state, action) => {
     const { all_products } = state;
     const { text, category, company, color, price, shipping } = state.filters;
     let tempProducts = [...all_products];
+    
     if (text) {
       tempProducts = tempProducts.filter(product => {
         return product.name.toLowerCase().startsWith(text);
       });
+    }
+
+    if (category !== 'all') {
+      tempProducts = tempProducts.filter(
+        product => product.category === category
+      );
+    }
+
+    if (company !== 'all') {;
+      tempProducts = tempProducts.filter(
+        product => product.company === company
+      );
+    }
+
+    if (color !== 'all') {
+      tempProducts = tempProducts.filter(product => {
+        return product.colors.find(c => c === color);
+      });
+    }
+
+    tempProducts = tempProducts.filter(product => product.price <= price);
+    if (shipping) {
+      tempProducts = tempProducts.filter(product => product.shipping === true);
     }
     return { ...state, filtered_products: tempProducts };
   }
