0230ee86481a60b7dec0af6a6fa0fb6e9e3ac262
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-25 09:30:37 +0200

    Update cart_context.js

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,18 +8,25 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions';
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart');
+  if (cart) {
+    return JSON.parse(localStorage.getItem('cart'));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 543,
 };
 
 const CartContext = React.createContext();
-
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
@@ -27,6 +34,10 @@ export const CartProvider = ({ children }) => {
   const removeItem = id => {};
   const toggleAmount = (id, value) => {};
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
@@ -35,7 +46,6 @@ export const CartProvider = ({ children }) => {
     </CartContext.Provider>
   );
 };
-
 export const useCartContext = () => {
   return useContext(CartContext);
 };
