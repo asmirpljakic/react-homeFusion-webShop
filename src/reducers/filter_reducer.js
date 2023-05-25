941135fe50a115e8e7ddf0c12ab246ff253e0182
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-25 07:42:45 +0200

    Update filter_reducer.js

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,7 +13,6 @@ const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     let maxPrice = action.payload.map(p => p.price);
     maxPrice = Math.max(...maxPrice);
-
     return {
       ...state,
       all_products: [...action.payload],
@@ -21,7 +20,6 @@ const filter_reducer = (state, action) => {
       filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
-
   if (action.type === SET_GRIDVIEW) {
     return { ...state, grid_view: true };
   }
@@ -31,7 +29,6 @@ const filter_reducer = (state, action) => {
   if (action.type === UPDATE_SORT) {
     return { ...state, sort: action.payload };
   }
-
   if (action.type === SORT_PRODUCTS) {
     const { sort, filtered_products } = state;
     let tempProducts = [...filtered_products];
@@ -53,48 +50,40 @@ const filter_reducer = (state, action) => {
     }
     return { ...state, filtered_products: tempProducts };
   }
-
   if (action.type === UPDATE_FILTERS) {
     const { name, value } = action.payload;
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
-
   if (action.type === FILTER_PRODUCTS) {
     const { all_products } = state;
     const { text, category, company, color, price, shipping } = state.filters;
     let tempProducts = [...all_products];
-    
     if (text) {
       tempProducts = tempProducts.filter(product => {
         return product.name.toLowerCase().startsWith(text);
       });
     }
-
     if (category !== 'all') {
       tempProducts = tempProducts.filter(
         product => product.category === category
       );
     }
-
-    if (company !== 'all') {;
+    if (company !== 'all') {
       tempProducts = tempProducts.filter(
         product => product.company === company
       );
     }
-
     if (color !== 'all') {
       tempProducts = tempProducts.filter(product => {
         return product.colors.find(c => c === color);
       });
     }
-
     tempProducts = tempProducts.filter(product => product.price <= price);
     if (shipping) {
       tempProducts = tempProducts.filter(product => product.shipping === true);
     }
     return { ...state, filtered_products: tempProducts };
   }
-
   if (action.type === CLEAR_FILTERS) {
     return {
       ...state,
