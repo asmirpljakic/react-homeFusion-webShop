255a3cc4acc96998f95d7cd32c1bf54a47471b49
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-25 07:40:59 +0200

    Update cart_context.js

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,13 +8,25 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions';
 
-const initialState = {};
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 543,
+};
 
 const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  };
+
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart }}>
+      {children}
+    </CartContext.Provider>
   );
 };
 export const useCartContext = () => {
