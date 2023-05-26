6f16e59cff1e9f0ee6413ff12d71ffbf6b8a2f5f
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-26 12:37:54 +0200

    Update CartButtons.js

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -20,16 +20,19 @@ const CartButtons = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type='button' className='auth-btn' onClick={loginWithRedirect}>
-        Login <FaUserPlus />
-      </button>
-      <button
-        type='button'
-        className='auth-btn'
-        onClick={() => logout({ returnTo: window.location.origin })}
-      >
-        Lgout <FaUserMinus />
-      </button>
+      {myUser ? (
+        <button
+          type='button'
+          className='auth-btn'
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   );
 };
