e539f25377a63a1eea5cd1a200fe31cdf7f79b76
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-26 08:50:04 +0200

    toggleAmount()

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -28,6 +28,7 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
@@ -35,7 +36,11 @@ export const CartProvider = ({ children }) => {
   const removeItem = id => {
     dispatch({ type: REMOVE_CART_ITEM, payload: id });
   };
-  const toggleAmount = (id, value) => {};
+
+  const toggleAmount = (id, value) => {
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+  };
+
   const clearCart = () => {
     dispatch({ type: CLEAR_CART });
   };
@@ -51,7 +56,6 @@ export const CartProvider = ({ children }) => {
     </CartContext.Provider>
   );
 };
-// make sure use
 export const useCartContext = () => {
   return useContext(CartContext);
 };
