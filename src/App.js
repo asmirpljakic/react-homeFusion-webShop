9bb9d78a8f97188d05ea80cb7e93b388091a10bb
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-28 08:43:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,31 +10,34 @@ import {
   About,
   Products,
   PrivateRoute,
+  AuthWrapper,
 } from './pages';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='products' element={<Products />} />
-        <Route path='products/:id' element={<SingleProduct />} />
-        <Route
-          path='checkout'
-          element={
-            <PrivateRoute>
-              <Checkout />
-            </PrivateRoute>
-          }
-        />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<SingleProduct />} />
+          <Route
+            path='checkout'
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
