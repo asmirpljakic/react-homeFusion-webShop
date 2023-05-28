25d0afb27ac4ba7368b74c628d35fbe05690663e
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-28 08:14:07 +0200

    Update PrivateRoute.js

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useUserContext } from '../context/user_context';
 
-const PrivateRoute = () => {
-  return <h4>Private Route</h4>;
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth0();
+  if (!user) {
+    return <Navigate to='/' />;
+  }
+  return children;
 };
 export default PrivateRoute;
