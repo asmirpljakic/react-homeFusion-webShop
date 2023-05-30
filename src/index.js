99408a44c160fc8cddb09b5438c3fe2d0783e76f
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-30 18:14:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Auth0Provider
-    domain='dev-64aecoq3tcmhzg7s.us.auth0.com'
-    clientId='w3aoglH5OstyVICb5AVgqVv99ImiA06l'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
