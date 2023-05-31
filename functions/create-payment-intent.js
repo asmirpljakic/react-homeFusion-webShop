25064d07391555a7aa6cc98cbbfa247ef10a15f0
Asmir Pljakic <asmirpljakic@gmail.com>
2023-05-31 13:42:08 +0200

    add create-payment-intent

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,7 @@
+exports.handler = async function (event, context) {
+  const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+  return {
+    statusCode: 200,
+    body: 'payment intent',
+  };
+};
