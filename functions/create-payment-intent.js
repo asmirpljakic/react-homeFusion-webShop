6db68d5e05749401b676152bcbfc87d08fa24c4c
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-01 07:46:13 +0200

    Update create-payment-intent.js

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,7 +1,14 @@
 exports.handler = async function (event, context) {
-  const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+  if (event.body) {
+    const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+    console.log(cart);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(cart),
+    };
+  }
   return {
     statusCode: 200,
-    body: 'payment intent',
+    body: 'Create Payment Intent',
   };
 };
