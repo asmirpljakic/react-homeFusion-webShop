b6a11dc90346f663e104fd15b6620ae0100f30e2
Asmir Pljakic <asmirpljakic@gmail.com>
2024-04-17 12:41:56 +0200

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -39,6 +39,7 @@ export const services = [
   },
 ];
 
-export const products_url = 'https://course-api.com/react-store-products';
+export const products_url = 'https://www.course-api.com/react-store-products';
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
+export const single_product_url = `https://www.course-api.com/react-store-single-product?id=`;
+
